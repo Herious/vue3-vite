d4309e469f452daddbf4dc0fbed8f55e6b03cea9
herious <909578167@qq.com>
2021-03-20 23:59:46 +0800

    配置vuex, router

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div> 123 </div>
+</template>
+
+<script>
+export default {
+ created() {
+  console.log(this.$store.state.count);
+  this.$store.commit('increment');
+  console.log(this.$store.state.count);
+ }
+}
+</script>
+
+<style>
+
+</style>
